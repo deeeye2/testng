6beaeb49ec26a0e0dd69a085d93aeeb1cee4b779
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-12-30 09:50:52 +0000

    Create HelloWorld.java

---
+++ src/com/example/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * A simple HelloWorld application.
+ */
+public class HelloWorld {
+
+    /**
+     * Returns a greeting message.
+     *
+     * @return a simple greeting.
+     */
+    public String getMessage() {
+        return "Hello, TestNG!";
+    }
+}
